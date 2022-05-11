bac4a24900b074b09813c0a6e76b4584f0079725
Nunthestar <n976apple@gmail.com>
2022-05-11 10:15:52 +0700

    upload

--- src/app/share-component/nav-menu/nav-menu.component.ts
+++ src/app/share-component/nav-menu/nav-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from 'src/app/service.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -8,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class NavMenuComponent implements OnInit {
   showSearch = false;
   lang = 'la';
-  constructor() { }
+
+    laoTitle = '';
+    engTitle = '';
+  constructor(
+    private serveic: ServiceService
+  ) { }
 
   ngOnInit(): void {
+    this.setTitle();
+  }
+
+  getMainUrl(): string{
+    return (this.serveic.baseURL === 'http://psldoic.gov.la/website/api/')? './assets/logo/logo_ponsaly.jpeg': (this.serveic.baseURL === 'http://odxdoic.gov.la/oudomxay/api/')? './assets/logo/logo_oudomsai.png' : './assets/logo/logo_leunumthar.jpeg';
+  }
+
+  setTitle(): void{
+     if(this.serveic.baseURL === 'http://psldoic.gov.la/website/api/'){
+        this.laoTitle = 'ພະແນກອຸດສາຫະກຳ ແລະ ການຄ້າ ແຂວງຜົ້ງສາລີ';
+        this.engTitle = 'Department of Industry and Commerce of Phongsaly Province';
+     }else if(this.serveic.baseURL === 'http://odxdoic.gov.la/oudomxay/api/'){
+      this.laoTitle = 'ພະແນກອຸດສາຫະກຳ ແລະ ການຄ້າ ແຂວງອຸດົມໄຊ';
+      this.engTitle = 'Department of Industry and Commerce of Oudomxay Province';
+     }else{
+      this.laoTitle = 'ພະແນກອຸດສາຫະກຳ ແລະ ການຄ້າ ແຂວງຫຼວງນ້ຳທາ';
+      this.engTitle = 'Department of Industry and Commerce of Luangnamtha Province';
+     }
+
+  }
+
+  checkMenu(): boolean{
+    return (this.serveic.baseURL === 'http://psldoic.gov.la/website/api/')? true: (this.serveic.baseURL === 'http://odxdoic.gov.la/oudomxay/api/')? false : false;
   }
 
 }
